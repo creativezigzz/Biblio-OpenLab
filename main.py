f558961f71b442a020cd3bf1de0f4a3ac7ea414c
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2022-11-09 01:57:04 +0200

    fixing issus

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 #!python3
-# -*- coding: utf-8 -*-
 
 import os  # To use delete and rename
 import csv  # To read csv and write on it
 import Books as b  # Import the class file of books who manage the creation and displaying of books
 import argparse  # Used to create interaction directly from the commandline
 
-def remove_book(title, catalogue):
-    """
-    function to remove rented book from library list
-    PRE: The library file must exists and title must be present in library
-    POST: The rented book is removed from library list
-    """
-    new_catalogue = list(filter(lambda x: x.title != title, catalogue))
-    with open ("library.csv", "w+") as file:
-        file.write("title, author")
-        for item in catalogue:
-            file.write(f"{item.title},{item.author}\n")
-    
-    
 
 def print_books(library='library.csv'):
     """
     Function to print all the books from a csv file called(library.csv)
-    PRE:the library file must exists.
+    PRE:the library file must exist.
     POST: Print all the books in the format : Title - Author
     RAISE: If no books in the library should raise an Exception : LibraryEmpty
     """
@@ -55,10 +41,9 @@ def take_a_book(title, library):
                         book_rented = b.Book(row[0], row[1])  # The book that we rent
                         print("{} has been successfully rented \n Thank you for using our library system".format(
                             book_rented))  # Print in the console the book that we rent
-                os.rename(library, "library.csv")
-                os.rename("temp.csv", library)
-                #os.remove("library.csv")
-                remove_book(title, csv_to_list_of_books(library))
+        os.rename(library, "dump.csv")
+        os.rename("temp.csv", library)
+        os.remove("dump.csv")
     else:
         # If the book is not in the library display some message
         print("\nThe book has not been found.\nLook for any typo in the title "
@@ -78,7 +63,7 @@ def is_in_library(title, list_of_books):
     """
     # For points : using lambda and filter fonction : map
     title_list = list(map(lambda x: x.title, list_of_books))  # We create a list contenting all the title of the books
-    
+
     if title in title_list:
         return True
     return False
