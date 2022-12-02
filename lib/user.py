43d24b7385c49fd237e40839cb0fa511a8b9465c
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2022-12-02 06:01:09 +0200

    user class

---
+++ lib/user.py
@@ -0,0 +1,19 @@
+
+
+class User():
+
+    def __init__(self,first_name,lastname):
+        pass
+
+    def check_account(self):
+        pass
+
+class Student(User):
+
+    # def __init__(self):
+    #     super(Student,self).__init__()
+     pass
+class Teacher(User):
+    # def __init__(self):
+    #     super(Teacher, self).__init__()
+    pass
